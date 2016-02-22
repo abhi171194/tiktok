95788d4496130500acbea7585a7a1a33af196e6d
kshitij <kshitij.jain.ece13@itbhu.ac.in>
2016-02-22 15:07:00 +0530

    connectwithserver mod-1

--- connextwithserver.py
+++ connextwithserver.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 language = [{"name" : "english" , "marks" : "100"},{"name" : "digital" , "marks" : "100"},{"name" : "maths" ,  "marks" : "100"},{"name" : "science", "marks" : "100"}]
 @app.route('/' , methods=['GET'])
 def home():
-    return jsonify({"name" : "kshitij"})
+    data = json.load(urllib2.urlopen("http://cricscore-api.appspot.com/csa"))
+    for node in data:
+        list1 =+ [{"team1" : node['t1']}]
+    return jsonify(list)
 
 @app.route('/firstpage', methods=['GET'])
 def page():
